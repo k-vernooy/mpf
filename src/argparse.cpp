e5196216708d01270ac9f9d0e248e658d5886c8c
quyer <kaivernooy.other.1@gmail.com>
2020-07-13 14:35:46 -0400

    Remove argparser class and validation definitions

--- src/argparse.cpp
+++ src/argparse.cpp
@@ -1,49 +1,16 @@
 #include <iostream>
 #include "../include/util.h"
-#include "../include/argparse.h"
+#include "../include/mpf.h"
 
 
 bool ValidateOrder(std::string arg) {
-    if (arg != "--order") {
-        Err("Passed arg is incorrect in ValidateOrder");
-        return false;
-    }
-    if (true) {
-        return true;
-    }
-    Err("Unanticipated error while parsing argument, exitting");
-    return false;
+    return true;
 }
-
 
 bool ValidateFilter(std::string arg) {
-    if (arg != "--order") {
-        Err("Passed arg is incorrect in ValidateOrder");
-        return false;
-    }
-    if (true) {
-        return true;
-    }
-    Err("Unanticipated error while parsing argument, exitting");
-    return false;
+    return true;
 }
 
-
-static std::map<std::string, CliArg> ARGLIST = {
-    {"--order", CliArg("--order", false, ValidateOrder)},
-    {"--filter", CliArg("--filter", false, ValidateFilter)}
-};
-
-
-ArgParser::ArgParser(int argc, char** argv) {
-    FilesList inputFiles = FilesList();
-    for (int i = 0; i < argc; i++) {
-        if (ARGLIST.find(argv[i]) != ARGLIST.end()) {
-            ARGLIST[argv[i]].validation(argv[i]);
-        }
-        else {
-            // is a file, add to FilesList
-            inputFiles.addFile(argv[i]);
-        }
-    }
+bool ValidateConfig(std::string arg) {
+    return true;
 }
