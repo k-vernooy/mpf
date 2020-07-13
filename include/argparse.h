08229ce0ec53fb592a081d5358db275532644925
k-vernooy <kai.a.vernooy@gmail.com>
2020-07-13 11:23:33 -0400

    Add music player classes for applcation

--- include/argparse.h
+++ include/argparse.h
@@ -2,6 +2,49 @@
 #include <vector>
 #include <map>
 
+static std::map<std::string, CliArg> ARGLIST;
+
+
+/**
+ * The music player. Lists the playable files,
+ * filters the files, opens a gui, and sends information
+ * to MPD using a separate driver.
+ */
+class MusicPlayer {
+    public:
+        static MusicPlayer CreateDefaultMP();
+        MPConfig configuration;
+        FilesList files;
+};
+
+/**
+ * A class representing a MusicPlayer configuration.
+ * Contains default variables, as well as
+ * filter and order vars used instantially.
+ */
+class MPConfig {
+    // A list of blank variables, to be read from
+    // config files or to be specified at runtime
+    std::map<std::string, std::string> VARIABLES = {
+        {"DEFAULT_DIR", ""},
+        {"DEFAULT_FILTER", ""},
+        {"DEFAULT_ORDER", ""},
+        {"FILTER", ""},
+        {"ORDER", ""}
+    };
+
+    public:
+        // Read and write to the ~/.music-filter.config file
+        static MPConfig ReadFromFile(std::string filePath);
+        void writeToFile(std::string filePath);         
+};
+
+
+/**
+ * Represents a command line argument.
+ * Contains information about each CLI argument
+ * as well as the value passed.
+ */
 class CliArg {
     public:
         CliArg(std::string name, bool isBool, bool (*validation)(std::string))
@@ -9,22 +52,50 @@ class CliArg {
 
         std::string name;
         bool isBool;
-        std::string passedValue;
+        bool boolVal;
+
+        int numVals;
+        int isInf;
+
+        std::vector<std::string> passedValue;
+
+        // validate the quality of the passed value
         bool (*validation)(std::string);
 };
 
 
-class FilesList {
-    std::vector<std::string> files;
-
-    public:
-        FilesList() {}
-        void addFile(std::string);
-        void removeFile(std::string);
+// All file formats supported by mpf
+enum FileFormat {
+    MP3, WAV, OGG
 };
 
 
-class ArgParser {
+/**
+ * Represents a physical and validated
+ * audio file. Can read tags from the file.
+ */
+class File {
+    std::string filePath;
+    std::string readID3Tag(std::string tagName);
+    bool validate();
+    FileFormat format;
+};
+
+
+/**
+ * A wrapper for a vector of Files.
+ * eCan be filtered and ordered by ID3v2 tags using id3lib
+ */
+class FilesList {
+    std::vector<File> files;
+    static bool ValidateFile(std::string);
+
     public:
-        ArgParser(int argc, char** argv);
+        FilesList() {}
+
+        bool validateAllFiles();
+        void addFile(std::string);
+        void removeFile(std::string);
+        void applyFilter(std::string);
+        void applyOrder(std::string);
 };
