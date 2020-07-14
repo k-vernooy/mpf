3977e3d410c9be959a1d34d4fda5e23255bf537d
k-vernooy <kai.a.vernooy@gmail.com>
2020-07-14 10:53:25 -0400

    Add filterStr parser

--- src/filesystem.cpp
+++ src/filesystem.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <algorithm>
+
+// mpf class + method declarations
 #include "../include/mpf.h"
 #include "../include/util.h"
 
+// for reading tag metadata
+#include <taglib/fileref.h>
+#include <taglib/tag.h>
+#include <taglib/tpropertymap.h>
+
+using std::cout;
+using std::endl;
 
 bool operator== (const File& f1, const File& f2) {
     return f1.filePath == f2.filePath;
@@ -10,6 +19,26 @@ bool operator== (const File& f1, const File& f2) {
 
 bool File::validate() {
     return (boost::filesystem::exists(filePath));
+}
+
+std::string File::readID3Tag(std::string tag) {
+    TagLib::FileRef f(filePath.c_str());
+
+    if (!f.isNull() && f.file()) {
+        TagLib::PropertyMap tags = f.file()->properties();
+        if (tags.find(tag) != tags.end()) {
+            if (tags[tag].size() > 1) Err("lots of vals");
+            return std::string(tags[tag][0].toCString());
+        }
+        else {
+            Err("Tag '" + tag + "' is not available in file '" + filePath + "'");
+            return "";
+        }
+    }
+    else {
+        Err("uncaught error reading tag '" + tag + "'");
+        return "";
+    }
 }
 
 void FilesList::addFile(File f) {
@@ -28,17 +57,44 @@ std::size_t FilesList::size() {
     return files.size();
 }
 
-void FilesList::applyFilterCmd(std::string lv, std::string rv, std::string comperator) {
+void FilesList::applyFilterCmd(std::string lv, std::string rv, std::string comparator) {
+    UpperCase(lv); UpperCase(rv);
+
+    for (size_t i = 0; i < files.size(); i++) {
+        std::string tagVal = files[i].readID3Tag(lv);
+        UpperCase(tagVal);
+        if (!tagVal.empty()) {
+            cout << tagVal << ", " << rv << ", " << comparator << endl;
+        }
+        else if (ARGLIST["--keep"].boolVal)
+            continue;
+        else
+            removeFile(files[i]);
+    }
     return;
 }
 
 void FilesList::applyFilter(std::string filter) {
     std::vector<std::string> cmds = Split(filter, ";");
+    std::vector<std::string> separators = {
+        "==", "<=", ">=", "<", ">"
+    };
+
     for (std::string cmd : cmds) {
-        // parse cmd into comperator, lv, and rv
-        // if (cmd.))
-            // this->applyFilterCmd(cmd);
-        std::cout << "Applying command " << cmd << std::endl;
+        std::string comperator, lv, rv;
+        for (std::string separator : separators) {
+            if (cmd.find(separator) != std::string::npos) {
+                comperator = separator;
+                std::vector<std::string> values = Split(cmd, comperator);
+                lv = values[0];
+                rv = Join(std::vector<std::string>(
+                    values.begin() + 1, values.end()
+                ), comperator);
+                break;
+            }
+        }
+
+        this->applyFilterCmd(lv, rv, comperator);
     }
 }
 
