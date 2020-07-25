19a4a8e4a91063a9e7157a8bba2d021af5b239be
k-vernooy <kai.a.vernooy@gmail.com>
2020-07-25 15:21:08 -0400

    Add gui app body and mainloop

--- src/gui.cpp
+++ src/gui.cpp
@@ -7,16 +7,19 @@
  * SDL_Mixer
  ***************************************/
 
+#include <iostream>
 #include "../include/mpf.h"
+#include "../include/util.h"
 
-
-static void GUI::SDL_DrawRoundedRect(SDL_Renderer* renderer, const SDL_Rect* r, const int br) {
+void GUI::SDL_DrawRoundedRect(SDL_Renderer* renderer, const SDL_Rect* r, const int br) {
     // SDL_SetRenderDrawColor(renderer,)
     SDL_RenderDrawRect(renderer, r);
 }
 
 void GUI::init() {
     if (isActive) return;
+    int SCREEN_WIDTH = 500,
+        SCREEN_HEIGHT = 500;
 
     // Initialize SDL video/audio
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
@@ -52,62 +55,102 @@ void GUI::init() {
 }
 
 void GUI::clear() {
-    SDL_SetRenderDrawColor(255, 255, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
 }
 
-void GUI::playAudio() {
-    // resume `currentFile` at `fProgress`
 
+bool AudioApp::advanceFile() {
+    currentFile++;
+    return (currentFile != files.size());
 }
 
-void pauseAudio() {
-    Mix_HaltMusic(-1);
+// void GUI::playAudio() {
+//     // resume `currentFile` at `fProgress`
+
+// }
+
+void AudioApp::pauseAudio() {
+    musicPaused = true;
+    Mix_HaltMusic();
 }
 
-void GUI::isPlayingAudio() {
-    return (Mix_PlayingMusic() || musicPaused)
+bool AudioApp::isPlayingAudio() {
+    return (Mix_PlayingMusic() || musicPaused);
 }
 
 
 void GUI::renderFrame() {
     // clear renderer
     SDL_RenderClear(renderer);
-    SDL_RenderPresent(render);
+    SDL_RenderPresent(renderer);
 }
 
 
-void GUI::beginGUI(MusicPlayer* player) {
+void GUI::run() {
+    files.applyFilter(config.getVariable("FILTER"));
     this->init();
-    while (!isActive) {
+
+    while (isActive) {
         // render the current state
         renderFrame();
 
-        // // handle all events
-        // SDL_Event evt;
-        // while (SDL_PollEvent(*evt)) {
-            
-        // }
+        // handle all events
+        SDL_Event evt;
+        while (SDL_PollEvent(&evt)) {
+            if (evt.type == SDL_QUIT) {
+                isActive = false;
+            }
+            if (evt.type == SDL_KEYDOWN) {
+                switch (evt.key.keysym.sym) {
+                    case SDLK_SPACE:
+                        if (musicPaused) {
+                            files.getFile(currentFile).beginPlayback(fProgress / 1000.0);
+                            musicPaused = false;
+                        }
+                        else {
+                            pauseAudio();
+                        }
+                        break;
+                    case SDLK_LEFT:
+                        fProgress -= 5000;
+                        if (fProgress < 0) fProgress = 0;
+                        Mix_SetMusicPosition(fProgress / 1000.0);
+                        break;
+                    case SDLK_RIGHT:
+                        fProgress += 5000;
+                        Mix_SetMusicPosition(fProgress / 1000.0);
+                        break;
+                }
+            }
+        }
 
-        // // check if we need to start a new song
-        // if (!isPlayingAudio()) {
-        //     if (!advanceFile()) {
-        //         cout << "Completed all files." << endl;
-        //         isActive = false;
-        //     }
-        //     else {
-        //         // begin playback of new audio file
-        //     }
-        // }
+        // check if we need to start a new song
+        if (!isPlayingAudio()) {
+            if (!advanceFile()) {
+                Log("Completed all files.");
+                isActive = false;
+            }
+            else {
+                Log("Advanced to file " + files.getFile(currentFile).filePath);
+                // begin playback of new audio file
+                fProgress = 0;
+                SDL_SetWindowTitle(window, files.getFile(currentFile).filePath.c_str());
 
-        // sleep FPS
-        usleep(500);
+                // begin playback
+                files.getFile(currentFile).beginPlayback(0);
+            }
+        }
+
+        if (!musicPaused)
+            fProgress += 1000.0 / (double)FPS;
+        SDL_Delay(1000.0 / FPS);
     }
 
     // SDL_UpdateWindowSurface(window);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
-    cout << "App terminated successfully." << endl;
+    Log("App terminated successfully.");
     return;
 }
